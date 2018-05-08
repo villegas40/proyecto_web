082d357f070cc700a343f484cd9bff0fb46a2551
Abiel Villegas <abiel.villegas17@tectijuana.edu.mx>
2018-05-08 09:33:05 -0700

    Diseño

--- sastreria/pagina/forms.py
+++ sastreria/pagina/forms.py
@@ -4,19 +4,23 @@ from .models import Perfil,Citas,Sastrerias
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm # Registrar usuario
 import datetime
+from django.utils.translation import gettext_lazy as _
 
 # Formulario de Registro
 class SignupForm(UserCreationForm):
     # user = forms.CharField(max_length = 20, required = True, help_text = 'User name max lenght 30 characters.')
     #bio = formsself.CharField(max_length = 500,  )
-    user_name = forms.CharField(max_length = 30, help_text = 'Enter first name, max 30 characters.')
-    user_last = forms.CharField(max_length = 30, help_text = 'Enter last name, max 30 characters.')
-    birth_date = forms.DateField(help_text = 'Required. Format: YYYY-MM-DD')
+    user_name = forms.CharField(max_length = 30, help_text = 'Ingrese su nombre, máximo 30 caracteres.')
+    user_last = forms.CharField(max_length = 30, help_text = 'Ingrese su apellido, máximo 30 caracteres.')
+    birth_date = forms.DateField(help_text = 'Formato Requerido: YYYY-MM-DD')
 
     class Meta:
         model = User
         fields = ('username', 'user_name', 'user_last', 'birth_date', 'email' , 'password1', 'password2', )
-
+        help_texts = {
+            'username': 'Máximo 150 caracteres.',
+            'email': 'Ingrese iun correo electronico valido.',
+        }
 
 # Editar Perfil
 class EditProfileForm(UserChangeForm):
